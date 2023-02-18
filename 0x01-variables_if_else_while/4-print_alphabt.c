b419303dc6478ae91c41b9a845c959a14cb1529e
Fiifikev <kevinaryeh02@gmail.com>
2023-02-17 17:19:25 -0800

    printalphabet4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main-this is a main function
+ *
+ * Return:Always 0
+ */
+
+int main(void)
+{char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+
+
+
+
+
+
+}
