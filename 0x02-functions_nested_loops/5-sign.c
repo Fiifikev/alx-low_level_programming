cc15c08b08774cd17cea5171ef2b2a3160334ecf
Fiifikev <kevinaryeh02@gmail.com>
2023-02-21 17:59:54 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - this is a function
+ * @n: function paarameter
+ * Return: 1,0 and -1
+ */
+
+
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf("+, ");
+return (1);
+}
+else if (n == 0)
+{
+printf("0, ");
+return (0);
+}
+else
+{
+printf("-, ");
+return (-1);
+}
+
+}
