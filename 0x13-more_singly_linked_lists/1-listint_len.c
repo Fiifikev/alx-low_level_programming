639d0f4859a72aa00eff98c98385bc2ffb898bf4
Fiifikev <kevinaryeh02@gmail.com>
2023-04-04 18:32:50 -0700

    list length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,19 +3,15 @@
 /**
  * listint_len - prints the number of elements
  * @h: pointer
- * Return:number of elements
+ * Return: number of elements
  */
-
-
-
-
 
 size_t listint_len(const listint_t *h)
 {
 	size_t nodelen = 0;
 
 	while (h != 0)
-	{	
+	{
 		nodelen++;
 		h = h->next;
 	}
