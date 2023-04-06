8cd9bd7430d47f8904b28dc36c6cc81456baf0d7
Fiifikev <kevinaryeh02@gmail.com>
2023-04-05 19:30:47 -0700

    sum lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * sum_listint - sum of all data in the list
+ * @head:pointer
+ * Return:sum
+ */
+
+
+
+
+int sum_listint(listint_t *head)
+{
+	listint_t *cpt;
+	int sum;
+
+	sum = 0;
+	cpt = head;
+
+	while (cpt != NULL)
+	{
+		sum += cpt->n;
+		cpt = cpt->next;
+	}
+	return (sum);
+}
+
+
