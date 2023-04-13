7e47b5a85d9ae7a17925d0768ade9b53185bea22
Fiifikev <kevinaryeh02@gmail.com>
2023-04-13 06:56:00 -0700

    prints to the POSIX std output

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+
+#endif
